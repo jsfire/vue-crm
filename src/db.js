b5770e50fe38e7fea31c5b2836796a06029145e5
timmilesdw <58149492+timmilesdw@users.noreply.github.com>
2020-03-23 18:48:53 +0500

    del API key

--- src/db.js
+++ src/db.js
@@ -2,13 +2,8 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgZgWP30C8O8RFGss0LQqRdAqFrlaE5r8",
-  authDomain: "vue-not-selling-programm.firebaseapp.com",
-  databaseURL: "https://vue-not-selling-programm.firebaseio.com",
-  projectId: "vue-not-selling-programm",
-  storageBucket: "vue-not-selling-programm.appspot.com",
-  messagingSenderId: "2757212586",
-  appId: "1:2757212586:web:05a857163e45ac7b19d3d3"
+  apiKey: "API_KEY",
+  projectId: "APP_ID",
 };
 
 // Get a Firestore instance
